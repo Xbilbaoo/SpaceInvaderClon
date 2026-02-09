c127381ec7a0d61331735cdf1f45aed37179771b
XBilbaoo <x_bilbaozam@fpzornotza.com>
2026-02-09 23:36:49 +0100

    Player added to the level

--- src/com/Zypher/SpaceInvaderClon/Main.java
+++ src/com/Zypher/SpaceInvaderClon/Main.java
@@ -1,4 +1,26 @@
 package com.Zypher.SpaceInvaderClon;
 
+import javax.swing.JFrame;
+import com.Zypher.SpaceInvaderClon.model.PlayerSpace;
+import com.Zypher.SpaceInvaderClon.view.GamePanel;
+
 public class Main {
+
+    public static void main(String[] args) {
+        // 1. CREAR EL MODELO (La Lógica)
+        // Creamos la nave en la posición 300, 500 con tamaño 50x50
+        PlayerSpace player = new PlayerSpace(300, 500, 50, 50);
+
+        // 2. CREAR LA VISTA (El Lienzo)
+        // Le pasamos el 'player' para que sepa qué dibujar.
+        GamePanel panel = new GamePanel(player);
+
+        // 3. CONFIGURAR LA VENTANA (JFrame)
+        JFrame window = new JFrame("Space Invaders Clon");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.add(panel); // Metemos el dibujo dentro de la ventana
+        window.setSize(800, 600);
+        window.setResizable(false);
+        window.setVisible(true); // ¡Acción!
+    }
 }
--- src/com/Zypher/SpaceInvaderClon/view/GamePanel.java
+++ src/com/Zypher/SpaceInvaderClon/view/GamePanel.java
@@ -1,4 +1,40 @@
 package com.Zypher.SpaceInvaderClon.view;
 
-public class GamePanel {
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import com.Zypher.SpaceInvaderClon.model.PlayerSpace; // Importante: Traemos la nave del otro paquete
+
+public class GamePanel extends JPanel {
+
+    // Referencia al jugador (para saber dónde pintarlo)
+    private PlayerSpace player;
+
+    /**
+     * Constructor
+     * @param player El objeto jugador que este panel debe dibujar
+     */
+    public GamePanel(PlayerSpace player) {
+        this.player = player;
+
+        // Opcional: Color de fondo del juego (Negro espacio)
+        this.setBackground(Color.BLACK);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // Borra lo anterior (limpia la pantalla)
+
+        // 1. Elegimos color
+        g.setColor(Color.GREEN); // O Color.RED, el que gustes
+
+        // 2. Dibujamos el rectángulo del jugador
+        // Le preguntamos al jugador: ¿Dónde estás? ¿Cuánto mides?
+        g.fillRect(
+                player.getxPos(),
+                player.getyPos(),
+                player.getWidth(),
+                player.getHeight()
+        );
+    }
 }
