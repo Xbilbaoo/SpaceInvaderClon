f101e70a1461c01cb6d12ddfbf44a61be379a6a7
XBilbaoo <x_bilbaozam@fpzornotza.com>
2026-02-10 21:55:02 +0100

    keypress method done

--- src/com/Zypher/SpaceInvaderClon/controller/KeyHandler.java
+++ src/com/Zypher/SpaceInvaderClon/controller/KeyHandler.java
@@ -1,4 +1,41 @@
 package com.Zypher.SpaceInvaderClon.controller;
 
-public class KeyHandler {
+import com.Zypher.SpaceInvaderClon.model.PlayerSpace;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyHandler extends KeyAdapter {
+
+    private PlayerSpace player;
+
+    public KeyHandler(PlayerSpace player) {
+
+        this.player = player;
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        switch (code) {
+
+            case (KeyEvent.VK_RIGHT):
+
+                this.player.setDx(5);
+                break;
+
+            case (KeyEvent.VK_LEFT):
+
+                this.player.setDx(-5);
+                break;
+
+            default:
+
+                this.player.setDx(0);
+                break;
+        }
+    }
 }
