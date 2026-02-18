1d4f69c715ca5f2dcd787b9599b4952389dcda23
XBilbaoo <x_bilbaozam@fpzornotza.com>
2026-02-18 16:06:23 +0100

    modeling the Alien class

--- src/com/Zypher/SpaceInvaderClon/model/Alien.java
+++ src/com/Zypher/SpaceInvaderClon/model/Alien.java
@@ -1,6 +1,45 @@
 package com.Zypher.SpaceInvaderClon.model;
 
-public class Alien {
+public class Alien extends Entity {
 
+    private int dx;
 
+    /**
+     *
+     * Constructor with all attributes.
+     *
+     * @param xPos   Initial X position of the entity
+     * @param yPos   Initial Y position of the entity
+     * @param width  Initial width of the entity
+     * @param height Initial height of the entity
+     */
+    public Alien(int xPos, int yPos, int width, int height) {
+
+        super(xPos, yPos, width, height);
+        this.dx = 0;
+
+    }
+
+    @Override
+    public void tick(int screenWidth) {
+
+        setDx(1);
+
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public void changeDirection() {
+        setDx(getDx() * -1);
+    }
+
+    public void goDown() {
+        setyPos(getyPos() - 1);
+    }
 }
